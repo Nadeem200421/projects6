9b8ad2023625a8fc62c48a7e2799e21b9088dac1
Nadeem200421 <mohammednadeemhussain8@gmail.com>
2025-12-27 17:34:56 +0530

    Update import paths in PostClassifiedPage component

--- app/post-classified/page.tsx
+++ app/post-classified/page.tsx
@@ -2,11 +2,10 @@
 
 import { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import type { RootState } from "@/redux/store";
+import type { RootState } from "../redux/store";
 import { useRouter } from "next/navigation";
-import { toggleTheme, restoreTheme } from "@/redux/features/themeSlice";
-import "@/styles/post-classified.css";
-
+import { toggleTheme, restoreTheme } from "../redux/features/themeSlice";
+import "../styles/post-classified.css";
 
 export default function PostClassifiedPage() {
   const dispatch = useDispatch();
